7ca82dc2823376912e7ce37745e0d73e7e5e0ac0
Peter <72684079+Trial062@users.noreply.github.com>
2020-10-24 18:01:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      menuItem = document.querySelectorAll('.menu_item');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+});
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+})
